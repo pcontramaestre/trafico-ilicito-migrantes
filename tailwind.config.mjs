e890bb18b0a0d5d84e1ab895253b89740c6f46f6
Pablo Contramaestre <pcontramaestre@gmail.com>
2024-07-02 14:39:13 -0400

    Ajustes de la seccion 1

---
+++ tailwind.config.mjs
@@ -0,0 +1,24 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			'azul': '#014979',
+			'azul6': '#D6EDFF',
+			'blanco': '#F6F4F3',
+		},
+		extend: {
+			fontFamily: {
+        sans: ['"Inter Variable"', ...defaultTheme.fontFamily.sans],
+				serif: ['inika',...defaultTheme.fontFamily.serif],
+      },
+			colors: {
+				'azul': '#014979',
+				'azul6': '#D6EDFF',
+				'blanco': '#F6F4F3',
+			},
+		},
+	},
+	plugins: [],
+}
